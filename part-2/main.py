c0001d5f8e88569c6e1d04b85050010bf763df1d
TSOlami <tijani.saheed@yahoo.com>
2024-07-24 17:29:17 +0100

    feat: Add main.py file with module import examples and function calls

---
+++ part-2/main.py
@@ -0,0 +1,22 @@
+#  Importing a module
+# import messages
+
+# messages.hello()
+# messages.bye()
+
+# Aliases:
+# import messages as msg
+
+# msg.hello()
+
+# Importing specific functions:
+# from messages import hello, bye
+
+# hello()
+# bye()
+
+# Wildcard import: for import all functions from a module
+# from messages import *
+
+# hello()
+# bye()
