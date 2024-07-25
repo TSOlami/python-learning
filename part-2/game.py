2a47f1488bce616a10cabe24853a2d6073885309
TSOlami <tijani.saheed@yahoo.com>
2024-07-25 16:10:20 +0100

    chore: Refactor rock-paper-scissors game implementation for scoring

--- part-2/game.py
+++ part-2/game.py
@@ -7,6 +7,9 @@ while True:
 
     player = None
 
+    player_points = 0
+    computer_points = 0
+
     while player not in choices:
         player = input("rock, paper or scissors? ").lower()
 
@@ -20,30 +23,42 @@ while True:
             print("Computer: ", computer)
             print("Player: ", player)
             print("You lose!")
+            computer_points += 1
+
         if computer == 'scissors':
             print("Computer: ", computer)
             print("Player: ", player)
             print("You win!")
+            player_points += 1
         
     elif player == 'paper':
         if computer == 'scissors':
             print("Computer: ", computer)
             print("Player: ", player)
             print("You lose!")
+            computer_points += 1
+
         if computer == 'rock':
             print("Computer: ", computer)
             print("Player: ", player)
             print("You win!")
+            player_points += 1
 
     elif player == 'scissors':
         if computer == 'rock':
             print("Computer: ", computer)
             print("Player: ", player)
             print("You lose!")
+            computer_points += 1
+
         if computer == 'paper':
             print("Computer: ", computer)
             print("Player: ", player)
             print("You win!")
+            player_points += 1
+
+    print("You: ", player_points)
+    print("Computer: ", computer_points)
 
     play_again = input("Do you want to play again? (yes/no) ").lower()
 
